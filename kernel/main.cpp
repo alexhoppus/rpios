2c61f1627c9e6b4fb9a11f93023500154ab8929a
Alex Yashchenko <alexhoppus111@gmail.com>
2017-01-15 21:41:06 +0300

    sched: Add app1 and app2 binaries
    Signed-off-by: Alex Yashchenko <alexhoppus111@gmail.com>

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -19,6 +19,8 @@ inline void global_constructors_init()
 	}
 }
 
+extern char _binary_app1_start[];
+extern char _binary_app2_start[];
 
 extern "C" void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
 {
---
+++ user/app2.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#define AUX_MU_IO_REG 0xf2215040
+static inline void mmio_write(uint32_t reg, uint32_t data)
+{
+	        *(volatile uint32_t *)reg = data;
+}
+
+static inline uint32_t mmio_read(uint32_t reg)
+{
+	        return *(volatile uint32_t *)reg;
+}
+
+/* Loop <delay> times in a way that the compiler won't optimize away. */
+static inline void delay(int32_t count)
+{
+	asm volatile("mov r0, %[count]; __delay_%=: subs r0, r0, #1; bne __delay_%=\n"
+			: : [count]"r"(count) : "cc");
+}
+
+extern "C" int puts(char *str);
+extern "C" int yield(void);
+
+int main(int argc, char **argv)
+{
+	while(1) {
+		puts((char*)"Hey i'am app2!\n");
+		delay(1000000);
+		yield();
+	}
+	//mmio_write(AUX_MU_IO_REG, '*');
+	return 0;
+}
